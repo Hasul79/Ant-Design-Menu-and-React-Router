3a9946a00951badb80d032213e994c230448ff40
Hasul79 <hatuk2@mail.ru>
2022-10-31 20:07:28 +0400

    add ant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+import {Menu} from "antd" ;
+import {Routes,Route, useNavigate} from "react-router-dom";
+import {HomeOutlined, DashboardOutlined, UnorderedListOutlined, UserOutlined, PoweroffOutlined} from "@ant-design/icons/lib/icons"
+// import { Footer, Header } from 'antd/lib/layout/layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex", flexDirection:"column", flex: 1, height: "100vh"}}>
+    <Header />
+    <div style={{display:"flex", flexDirection:"row", flex: 1}}>
+      <SideMenu />
+      <Content />
+     </div>
+     <Footer />
+     </div>
+
+  );    
+}
+function Header(){
+  return (
+  <div style={{
+       height:60, 
+       backgroundColor:"lightskyblue",
+       color:"white",
+       fontSize: 30,
+       fontWeight: 700,
+       display:"flex",
+       justifyContent:"center",
+       alignContent:"center",
+       letterSpacing:2, 
+       textTransform:"uppercase" }}>
+     Header
     </div>
   );
 }
+function Footer(){
+  return (
+  <div style={{
+    height:60, 
+    backgroundColor:"lightgray",
+    color:"black",
+    fontSize: 32,
+    fontWeight: 700,
+    display:"flex",
+    justifyContent:"center",
+    alignContent:"center",
+    letterSpacing:2, 
+    textTransform:"uppercase" }}>
+  Footer
+  </div>
+  );
+}
+
+function SideMenu() {
+  const navigate = useNavigate();
+
+  return (
+     
+  <Menu 
+  onClick={({key}) => {
+   if(key === "signout"){
+    //TODO, sign put feature here
+  }else{
+    navigate(key)
+   }
+  }}
+  defaultSelectedKeys={[window.location.pathname]}
+  items={[
+    {label: "Home",
+       key:"/", 
+     icon:<HomeOutlined />},
+
+    {label: "Dashboard",
+       key:"/dashboard", 
+      icon: <DashboardOutlined />},
+
+    {label: "Users List",
+       key:"/usersList",
+      icon: <UnorderedListOutlined/>,
+     children:[
+     {label:'Active Users',
+    key: "/activeUsers"},
+    {label: 'Disabled users',
+     key: "/disabledUsers"}
+     ]
+    },
+      
+    {label: "Profile",
+       key:"/profile", 
+      icon: < UserOutlined/>},
+
+    {label: "Signout",
+       key:"/signout", 
+      icon: <PoweroffOutlined />,
+    danger: true },
+  ]}>
+  </Menu>
+  );
+}
+function Home(){
+return <div>Home Component</div>
+}
+function Content() {
+  return ( 
+  <div> 
+   <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/dashboard' element={<div>Dashboard</div>}></Route>
+    <Route path='/activeUsers' element={<div>Active Users List</div>}></Route>
+    <Route path='/disabledUsers' element={<div>Disabled Users List</div>}></Route>
+    <Route path='/profile' element={<div>Profile</div>}></Route>
+    <Route path='/signout' element={<div>Signout</div>}></Route>
+    </Routes>
+  </div>
+);
+}
+
 
 export default App;
